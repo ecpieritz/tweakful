153e764110b54c4a501f9b4193113e38ce13a3a1
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2023-01-08 13:01:53 -0300

    Added router to Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './scss/main.scss';
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Services from './pages/Services'
+import Contact from './pages/Contact'
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <p>teste</p>
-    </div>
+    <>
+      <Navbar />
+      <main>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/services' element={<Services />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
+      </main>
+    </>
   );
 }
 
