9deef19cd3220cf53561c002800a30755f46603a
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2023-01-11 12:07:22 -0300

    Added About Video section

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import BookAMeeting from '../../components/BookAMeeting'
 import Header from '../../components/Pages/Header'
+import Video from '../../components/Pages/Video'
 
 const About = () => {
   return (
@@ -9,6 +10,7 @@ const About = () => {
         title="We have created a big library of illustrations with custom style and action types."
         description="E-commerce Illustration Pack collects different ways to do quick shopping, pay in a second, and get all those stuff you need. There are smart delivery and polite service in the scenes. Each of them is realized in cold and warm colors with"
       />
+      <Video />
       <BookAMeeting />
     </>
   )
