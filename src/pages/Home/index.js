2eac91d5a6045615ac97b13fda272ca0c9c023a8
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2023-01-11 09:45:57 -0300

    Added Book A Meeting section

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,6 +3,7 @@ import HomeHeader from '../../components/HomeHeader'
 import VideoSection from '../../components/VideoSection'
 import Topics from '../../components/TopicsSection'
 import Goals from '../../components/Goals'
+import BookAMeeting from '../../components/BookAMeeting'
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
      <VideoSection />
      <Topics />
      <Goals />
+     <BookAMeeting />
     </>
   )
 }
