5f0166cca00110178f72156f7e88b0e0644db09e
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2023-01-18 09:16:04 -0300

    Added final tweaks

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import ContactUs from '../../components/Pages/Contact'
-import Map from '../../components/Pages/Map'
 
 const Contact = () => {
   return (
     <>
      <ContactUs />
-     <Map />
     </>
   )
 }
